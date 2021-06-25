ddbe5d5f674fe0ca05dcbf20e40ee5c741214e90
nickd <nickdiaz58@gmail.com>
2021-06-25 15:35:34 -0400

    Initial backend structure, models migrations controllers and serializers

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding."
+
+User.destroy_all
+Game.destroy_all
+
+u1 = User.create(name: "Kirk")
+u2 = User.create(name: "Spock")
+
+g1 = Game.create(completed: true, won: true)
+g2 = Game.create(completed: true, won: false)
+g3 = Game.create(completed: false, won: false)
+g4 = Game.create(completed: true, won: true)
+g5 = Game.create(completed: true, won: true)
+
+u1.games.push(g1, g2, g3)
+u2.games.push(g4, g5)
+
+puts "Seeding complete."
