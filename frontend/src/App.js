6a7b3f2bdab3b9efacc002ee85e6651a17c280d3
nickd <nickdiaz58@gmail.com>
2021-06-27 14:11:29 -0400

    Initial setup of routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
-import WelcomeScreen from './components/welcomeScreen'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
+import WelcomeScreen from './components/welcome/welcomeScreen'
+import GameContainer from './components/game/gameContainer';
+import EndScreen from './components/end/endScreen';
+//fix routes so player proceeds into game when submitting name form on welcome screen
+//player should proceed to end screen route after game completion
+//nest game route to be /user/id:/play, same with end screen
 
 function App() {
   return (
     <div className="App">
       <main>
-        <WelcomeScreen/>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <WelcomeScreen/>
+          </Route>
+          <Route path='/play'>
+            <GameContainer/>
+          </Route>
+          <Route path='/end'>
+            <EndScreen/>
+          </Route>
+        </Switch>
+      </Router>
       </main>
     </div>
   );
---
+++ frontend/src/components/end/endScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class EndScreen extends Component {
+
+    render() {
+        return(
+            <div>
+                <h1>End Screen</h1>
+            </div>
+        )
+    }
+}
